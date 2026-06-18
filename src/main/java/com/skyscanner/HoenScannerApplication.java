1f1a5ef75e1ae54761ffa8a7c8b3d093646a2850
ZhihaoTang615 <zhihao.tang@my.jcu.edu.au>
2026-06-18 19:14:48 +0800

    Implement search microservice

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,11 +1,15 @@
 package com.skyscanner;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
 
-public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
     public static void main(final String[] args) throws Exception {
         new HoenScannerApplication().run(args);
     }
@@ -17,12 +21,39 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
 
     @Override
     public void initialize(final Bootstrap<HoenScannerConfiguration> bootstrap) {
-
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
+    public void run(final HoenScannerConfiguration configuration, final Environment environment) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
 
+        List<SearchResult> carResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("rental_cars.json"),
+                        SearchResult[].class
+                )
+        );
+
+        for (SearchResult result : carResults) {
+            result.setKind("rental_car");
+        }
+
+        List<SearchResult> hotelResults = Arrays.asList(
+                mapper.readValue(
+                        getClass().getClassLoader().getResource("hotels.json"),
+                        SearchResult[].class
+                )
+        );
+
+        for (SearchResult result : hotelResults) {
+            result.setKind("hotel");
+        }
+
+        List<SearchResult> searchResults = new ArrayList<>();
+        searchResults.addAll(carResults);
+        searchResults.addAll(hotelResults);
+
+        final SearchResource resource = new SearchResource(searchResults);
+        environment.jersey().register(resource);
     }
-
 }
